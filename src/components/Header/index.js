201f8709efd866236fc1487c66d562fa4e2f865a
Gopal Patel <patelgopalk1@gmail.com>
2021-05-08 17:59:47 -0400

    Added header

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className='container-fluid'>
+            <header>
+                <h1>Employee Directory</h1>
+                <h6>Click on carrots to filter by heading or use the search box to narrow your results</h6>
+            </header>            
+        </div>
+    );
+}
+
+export default Header;
