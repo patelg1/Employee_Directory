597ea003807e17f3b078cab8eae9962d9c32524e
Gopal Patel <patelgopalk1@gmail.com>
2021-05-09 08:57:42 -0400

    Added search bar

---
+++ src/components/Search/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Search(props) {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light justify-content-center">
+        <form className="form-inline m-2">
+          <input
+            className="form-control mr-sm-2"
+            value={props.value}
+            name="search"
+            onChange={props.handleInputChange}
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />          
+        </form>
+      </nav>
+    </div>
+  );
+}
+
+export default Search;
